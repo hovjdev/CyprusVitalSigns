df62bd1b1d618bd73a39a1698e45c32d0c025a57
unknown <hovjdev@gmail.com>
2022-06-09 14:14:03 +0300

    code

--- cyprus_analytics.py
+++ cyprus_analytics.py
@@ -1,26 +1,33 @@
 import os
+import re
+import copy
 import random
 from re import I
 import wbgapi as wb
 import pandas as pd
 import seaborn as sns
 import numpy as np
-import matplotlib.pyplot as plt
+
 from sklearn.linear_model import LinearRegression
 from sklearn.covariance import EllipticEnvelope
+
+
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.lines as mlines
 
 from tools.plot_tools import prep_plot
 
 CO2_DATA_URL ="https://gml.noaa.gov/webdata/ccgg/trends/co2/co2_annmean_mlo.txt"
 ECONOMIES_1 = ['GRC',  'CYP', 'FRA', 'WLD']
 ECONOMIES_1 = ['GRC', 'CYP', 'WLD']
-ECONOMIES_2 = ['CYP', 'WLD']
+ECONOMIES_2 = ['CYP', 'GRC', 'FRA', 'WLD']
 SERIES = ['NY.GDP.PCAP.CD', 'SP.POP.TOTL']
 OUTPUT_DIR = 'output/cyprus_analytics'
 DEBUG=False
 
 
-MAX_IND_LENGTH = 50
+MAX_IND_LENGTH = 55
 MAX_IND = 5
 MIN_NB_DATA=15
 MAX_NB_OUTLIERS=2
@@ -242,7 +249,7 @@ def get_random_data(economies,
     if DEBUG:
         print(inds)
     
-    inds=filter_inds_on_length(inds, max_ind=10*max_ind, max_ind_length=max_ind_length)
+    inds=filter_inds_on_length(inds, max_ind=20*max_ind, max_ind_length=max_ind_length)
     assert len(inds)>0
     print('1 >>> inds:', inds)
 
@@ -268,35 +275,81 @@ def plot_df(df,title, economies, index, output_dir):
     df.columns = df.columns.str.replace("YR", "")
     df=df.T
 
+    #print(">>>>>>>> plot_df")
+    #print(df)
+
+    countries_nocyp=[]
+    countries_cyp=[]
+
     for c in economies:
         name=get_economie_name(c)
         df.columns = df.columns.str.replace(c, name)
+        if c == 'CYP':
+            countries_cyp.append(name)
+        else:
+            countries_nocyp.append(name)
 
-    linewidth=10
-    font_scale=4
+    print(f'>>> countries_nocyp: {countries_nocyp}')
+    print(f'>>> countries_cyp: {countries_cyp}')
+
+
+    linewidth=5
+    font_scale=3
+
     prep_plot(font_scale=font_scale)
-    ax=sns.lineplot(data=df, linewidth=linewidth, dashes=False)
+    plt.rcParams["figure.figsize"] = [16, 9]
+
+    g1=sns.lineplot(data=df[countries_nocyp], legend=True,  sizes=(10, 400),linewidth=linewidth, dashes=False)
+    box = g1.get_position()
+    g1.set_position([box.x0, box.y0, box.width * 0.9, box.height])
+
+    g2=sns.lineplot(data=df[countries_cyp],  legend=False,   sizes=(10, 400),  palette=['orange'], linewidth=linewidth, dashes=False)
+    box = g2.get_position()
+    g2.set_position([box.x0, box.y0, box.width * 0.9, box.height])
+
+    handles, _ = g2.get_legend_handles_labels()
+    line = mlines.Line2D([], [], color='orange', label='Cyprus', linewidth=linewidth)
+    handles.insert(0, line)
+   
+    for h in handles:
+        h.set_linewidth(linewidth)
+
+    plt.legend(handles=handles, loc='center right', bbox_to_anchor=(1.35, 0.5), ncol=1)
+
+
+    ax=g2
 
     #title = title.replace(', ', ',\n')
     ax.set_title(title)
 
     nbticks=6
-    nbt = int(len(ax.xaxis.get_ticklabels())/nbticks)+1
-    for i, tick in enumerate(reversed(ax.xaxis.get_ticklabels())):
-        if i % nbt != 0:
-            tick.set_visible(False) 
-    '''nbt = int(len(ax.yaxis.get_ticklabels())/nbticks)+1     
-    for i, tick in enumerate(reversed(ax.yaxis.get_ticklabels())):
-        if i % nbt != 0:
-            tick.set_visible(False)         
-    '''
+    ntx=len(ax.xaxis.get_ticklabels())
+    if ntx>nbticks:
+        nbt = int(ntx/nbticks)+1
+        for i, tick in enumerate(reversed(ax.xaxis.get_ticklabels())):
+            if i % nbt != 0:
+                tick.set_visible(False)
+    
+    nty=len(ax.xaxis.get_ticklabels())
+    if nty>nbticks:
+        nbt = int(nty/nbticks)+1     
+        for i, tick in enumerate(reversed(ax.yaxis.get_ticklabels())):
+            if i % nbt != 0:
+                tick.set_visible(False)          
 
-    leg = ax.legend()
-    for line in leg.get_lines():
-        line.set_linewidth(linewidth)
+    # plot the legend
+    output_png=os.path.join(output_dir, f'image{index}.png')
 
-    ax.figure.savefig(os.path.join(output_dir, f'image{index}.png'))
-    plt.close(ax.figure)
+    plt.savefig(output_png, format='png', dpi=600)
+    show=False
+    if show:
+        plt.show()
+    plt.close('all')
+    plt.clf()
+
+
+
+
 
 def create_media(data, economies, output_dir=OUTPUT_DIR):
 
@@ -347,13 +400,15 @@ if __name__ == "__main__":
             create_dir(OUTPUT_DIR)
             data = get_random_data(economies=ECONOMIES_1)
             print(data)
-            create_media(data=data, economies=ECONOMIES_2, output_dir=OUTPUT_DIR)
         except Exception as e:
             print(str(e))
             print(f"Try {i} failed")
             print(data)
             continue
+        create_media(data=data, economies=ECONOMIES_2, output_dir=OUTPUT_DIR)
         break
+        
+
 
 
 
