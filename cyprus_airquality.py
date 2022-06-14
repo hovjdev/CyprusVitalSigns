8044f4d4f91646cf110a2f886e7548e9fb9209d7
unknown <hovjdev@gmail.com>
2022-06-14 12:46:08 +0300

    code

--- cyprus_airquality.py
+++ cyprus_airquality.py
@@ -13,7 +13,9 @@ import matplotlib as mpl
 mpl.use('Agg')
 
 from mpl_toolkits.basemap import Basemap
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 import matplotlib.font_manager as fm
+import matplotlib.cm as cm
 from pytz import timezone
 
 OUTPUT_DIR = 'output/cyprus_airquality'
@@ -48,15 +50,15 @@ def delete_previous_files(path):
 
 
 pollutants_list = [
-#{"code":  38, "label": "NO", "fullname" :"pollutant_38", "index":1, "color": 'lightgreen', "label_en": "Nitrogen monoxide (air)", "label_el":"Μονοξείδιο Αζώτου", levels=[-1,-1,-1]},
-#{"code": 8, "label": "NO₂", "fullname" :	"pollutant_8", "index":2 , "color": 'aqua', "label_en": "Nitrogen dioxide (air)", "label_el":"Διοξείδιο Αζώτου", levels=[-1,-1,-1]},
-#{"code": 9, "label": "NOx", "fullname" :"pollutant_9", "index":3, "color": 'violet' , "label_en": "Nitrogen oxides (air)", "label_el":"Οξείδια του Αζώτου", levels=[-1,-1,-1]},
-#{"code": 1, "label": "SO₂", "fullname" :"pollutant_1", "index":4, "color": 'orange' , "label_en": "Sulphur dioxide (air)", "label_el":"Διοξείδιο Θείου", levels=[-1,-1,-1]},
+#{"code":  38, "label": "NO", "fullname" :"pollutant_38", "index":1, "color": 'lightgreen', "label_en": "Nitrogen monoxide (air)", "label_el":"Μονοξείδιο Αζώτου", "levels":[-1,-1,-1]},
+{"code": 8, "label": "NO₂", "fullname" :	"pollutant_8", "index":2 , "color": 'aqua', "label_en": "Nitrogen dioxide (air)", "label_el":"Διοξείδιο Αζώτου", "levels":[100, 150, 200]},
+#{"code": 9, "label": "NOx", "fullname" :"pollutant_9", "index":3, "color": 'violet' , "label_en": "Nitrogen oxides (air)", "label_el":"Οξείδια του Αζώτου", "levels":[-1,-1,-1]},
+#{"code": 1, "label": "SO₂", "fullname" :"pollutant_1", "index":4, "color": 'orange' , "label_en": "Sulphur dioxide (air)", "label_el":"Διοξείδιο Θείου", "levels":[-1,-1,-1]},
 {"code": 7, "label": "O₃", "fullname" :"pollutant_7", "index":5, "color": 'green', "label_en": "Ozone (air)", "label_el":"Όζον", "levels":[100,140,180]},
-#{"code": 10, "label": "CO", "fullname" :"pollutant_10", "index":6, "color": 'gray' , "label_en": "Carbon monoxide (air)", "label_el":"Μονοξείδιο Άνθρακα", levels=[-1,-1,-1]},
-#{"code": 5, "label": "PM₁₀", "fullname" :	"pollutant_5", "index":7, "color": 'blue' , "label_en": "Particulate matter < 10 μm (aerosol)", "label_el":"Σωματίδια < 10 μm", levels=[-1,-1,-1]},
+{"code": 10, "label": "CO", "fullname" :"pollutant_10", "index":6, "color": 'gray' , "label_en": "Carbon monoxide (air)", "label_el":"Μονοξείδιο Άνθρακα", "levels":[7000,15000,20000]},
+#{"code": 5, "label": "PM₁₀", "fullname" :	"pollutant_5", "index":7, "color": 'blue' , "label_en": "Particulate matter < 10 μm (aerosol)", "label_el":"Σωματίδια < 10 μm", "levels":[-1,-1,-1]},
 {"code": 6001, "label": "PM₂.₅", "fullname" :"pollutant_6001", "index":8 , "color": 'red', "label_en": "Particulate matter < 2.5 μm (aerosol)", "label_el":"Σωματίδια < 2.5 μm", "levels":[25,50,100]},
-#{"code": 20, "label": "C₆H₆", "fullname" :	"pollutant_20", "index":9, "color": 'magenta' , "label_en": "Benzene (air)", "label_el":"Βενζόλιο", levels=[-1,-1,-1]}
+#{"code": 20, "label": "C₆H₆", "fullname" :	"pollutant_20", "index":9, "color": 'magenta' , "label_en": "Benzene (air)", "label_el":"Βενζόλιο", "levels":[-1,-1,-1]}
 ]
 
 
@@ -94,15 +96,17 @@ for p in pollutants_list:
     pollutant_fullname=p['fullname']
     pollutant_levels=p["levels"]
 
-    plt.figure(figsize=FIGSIZE, dpi=MYDPI)
+    #figure = plt.figure(figsize=FIGSIZE, dpi=MYDPI)
+    figure, axes = plt.subplots(1, 1, figsize=FIGSIZE, dpi=MYDPI)
     fm.fontManager.addfont("input/fonts/bauhaus/BauhausRegular.ttf")
     plt.rcParams['font.family'] = 'Bauhaus'
 
-    m = Basemap(llcrnrlat=34.5, urcrnrlat=35.8,
-                llcrnrlon=32.2, urcrnrlon=34.6,
+    m = Basemap(llcrnrlat=34.45, urcrnrlat=35.75,
+                llcrnrlon=32, urcrnrlon=34.62,
                 epsg=4230,
                 projection='cyl',
-                resolution='c' )
+                resolution='c',
+                ax=axes)
 
 
     m.drawcoastlines(color='#6D5F47', linewidth=.4)
@@ -184,7 +188,52 @@ for p in pollutants_list:
                 weight="bold"
             )
 
-        plt.title(f"Air Quality in Cyprus: {pollutant_label}", fontsize = 40, y=1.0, pad=-45)
+        plt.title(f"Air Quality in Cyprus: {pollutant_label}", fontsize = 30, y=1.0, pad=-45)
+
+
+
+
+
+    cmap = mpl.colors.ListedColormap(['green', 'green', 'orange', 'orange', 'red', 'red', 'purple'])
+    bounds = [0, 
+              pollutant_levels[0]/2, 
+              pollutant_levels[0] , 
+              (pollutant_levels[0]+pollutant_levels[1])/2, 
+              pollutant_levels[1],
+              (pollutant_levels[1]+pollutant_levels[2])/2,
+              pollutant_levels[2],
+              pollutant_levels[2]*1.2]
+    labels = [ 
+              "Good",
+              pollutant_levels[0] , 
+              "Moderate",
+              pollutant_levels[1],
+              "Unhealthy",  
+              pollutant_levels[2]]              
+    ticks = bounds[1:-1]
+    norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
+
+
+    cb=m.colorbar( cmap=cmap,norm=norm, location="left",
+        boundaries= bounds,
+        ticks=ticks,
+        spacing='proportional',
+        fraction=0.046, pad=0)
+
+    cb.ax.set_yticklabels(labels, va="center")
+    cb.ax.tick_params(size=0)
+
+
+    for i, t in enumerate(cb.ax.get_yticklabels()):
+        t.set_fontsize(20)
+        t.set_rotation(90)
+        if i%2 == 0:
+            t.set_rotation(90)
+            t.set_fontsize(15)
+            t.set_x(t._x-.8)
+
 
     plt.savefig(os.path.join(OUTPUT_DIR, f'airquality_{pollutant_code}.png'), 
             bbox_inches='tight', dpi='figure',pad_inches=-.05)
+
+    
