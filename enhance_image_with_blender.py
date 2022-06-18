9de8668ae235175b393701ada5cf8e31beed7c49
hovjdev <hovjdev@gmail.com>
2022-06-18 19:23:03 +0300

    code

--- enhance_image_with_blender.py
+++ enhance_image_with_blender.py
@@ -1,3 +1,12 @@
 import os
 
+BLENDER="blender"
 IMAGE_PATH=""
+OUTPUT_DIR="output/blender"
+INPUT_IMAGE=""
+INPUT_BLENDER_FILE="input/blender/p1.blend"
+INPUT_BLENDER_SCRIPT="input/blender/script1.py"
+OUTPUT_IMAGE="frame_####.png"
+
+cmd = f"{BLENDER} -b {INPUT_BLENDER_FILE} --python {INPUT_BLENDER_SCRIPT} -o {OUTPUT_DIR}/{OUTPUT_IMAGE} -a"
+os.system(cmd)
