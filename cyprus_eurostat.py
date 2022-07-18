1406a776cd83c28fa940c59434fc58068f723f99
unknown <hovjdev@gmail.com>
2022-07-18 13:37:06 +0300

    code

--- cyprus_eurostat.py
+++ cyprus_eurostat.py
@@ -542,7 +542,7 @@ if __name__ == "__main__":
                 'title_short':"Nights spent",
                 'unit': ""},
         'tour_dem_extot' : {
-                'filters': {'GEO': ['CY'], 'UNIT': ['EUR'], 'FREQ': ['A'], 'EXPEND':['TOTAL'], 'PARTNER':['WORLD'], 'DURANTION':['N_GE1']}, 
+                'filters': { 'GEO': ['CY'], 'UNIT': ['EUR'] },  # 'EXPEND':['TOTAL'], 'FREQ': ['A'],'PARTNER':['WORLD'], 'PURPOSE':['TOTAL'],  'DURANTION':['N_GE1'], 'PARTNER':['DOM']
                 'title':"Nights spent at tourist accommodation establishments (monthly data)",
                 'title_short':"Nights spent",
                 'unit': ""}
