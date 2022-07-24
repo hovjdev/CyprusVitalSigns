e97930ae188b8280df79fe1c89ec88c643091ce4
hovjdev <hovjdev@gmail.com>
2022-07-24 21:53:43 +0300

    code

--- cyprus_sound.py
+++ cyprus_sound.py
@@ -2,6 +2,9 @@
 import os
 import argparse
 import random
+import shutil
+
+
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -58,11 +61,7 @@ def interpolate(y):
 
 
 
-def create_sound(nyp_files, output_dir):
-
-    random.shuffle(nyp_files)
-    nyp_files=nyp_files[:MAXFILES]
-
+def create_wave_file(nyp_files, output_wav):
     YYs=[]
     for f in nyp_files:
         print(f)
@@ -126,32 +125,115 @@ def create_sound(nyp_files, output_dir):
     beat_track = ['bass drum 1'] + bass_drum
     #multitrack_data_with_instruments.append(beat_track)
 
-    output_midi = os.path.join(OUTPUT_DIR, "sound.midi")
+    output_midi = output_wav[:-4] + ".midi"
     sonify.create_midi_from_data(multitrack_data_with_instruments, 
                         outputfile=output_midi,
                         track_type='multiple', key='c_sharp_major')
 
     if os.path.exists(output_midi):
-        output_wav = os.path.join(OUTPUT_DIR, "1_sound.wav")
         fs = FluidSynth()
         fs.midi_to_audio(output_midi, output_wav)
         audio = AudioSegment.from_wav(output_wav)
         audio=audio.normalize()
-        audio=audio-5
+        audio=audio-1
         fade_time=300
         audio = audio.fade_in(fade_time).fade_out(fade_time)
-        audio.export(output_wav)
+        audio.export(output_wav, "wav")
+        if os.path.exists(output_wav):
+            return output_wav
+    
+    return None
 
 
+
+def create_sound(nyp_files, output_dir):
+
+    random.shuffle(nyp_files)
+    nyp_files=nyp_files[:MAXFILES]
+
+    nb_nyp_files=len(nyp_files)
+    nyp_files_left = nyp_files[:int(nb_nyp_files/2)]
+    nyp_files_right = nyp_files[int(nb_nyp_files/2):]
+
+    try:
+        assert len(nyp_files_left) > 0 and len(nyp_files_right) > 0
+    except Exception as e:
+        nyp_files_left=nb_nyp_files
+        nyp_files_right=nb_nyp_files
+        print(str(e))
+    
+    tmp_path=os.path.join(OUTPUT_DIR, "TMP")
+    create_dir(tmp_path)
+    output_wav_left = os.path.join(tmp_path, "1_sound_left.wav")
+    output_wav_right = os.path.join(tmp_path, "1_sound_right.wav")
+
+    output_wav_left=create_wave_file(nyp_files_left, output_wav_left)
+    output_wav_right=create_wave_file(nyp_files_right, output_wav_right)
+
+    print(output_wav_left)
+    print(type(output_wav_left))
+    print(output_wav_right)
+    print(type(output_wav_right))
+
+
+    left_channel_as = AudioSegment.from_wav(output_wav_left)
+    right_channel_as = AudioSegment.from_wav(output_wav_right)
+    left_channel=np.array(left_channel_as.get_array_of_samples())
+    right_channel=np.array(right_channel_as.get_array_of_samples())
+    #print(left_channel.dtype, right_channel.dtype)
+    
+    def count_samples(left_channel, right_channel):
+        lr=right_channel.shape[0]
+        ll=left_channel.shape[0] 
+        #print(ll, lr)
+        return ll, lr
+    
+    ll, lr = count_samples(left_channel, right_channel)
+    l=max(ll, lr)
+    channels=np.zeros((2,l), dtype='int16')
+    channels[0,:ll]=left_channel
+    channels[1,:lr]=right_channel
+    left_channel_tmp=channels[0,:]
+    right_channel_tmp=channels[1,:]
+
+    a=np.sin(np.arange(0,l)*2*np.pi/l)**2
+    b=np.cos(np.arange(0,l)*2*np.pi/l)**2
+    left_channel=a*left_channel_tmp + b*right_channel_tmp
+    left_channel=left_channel.astype('int16')
+    right_channel=a*right_channel_tmp + b*left_channel_tmp
+    right_channel=right_channel.astype('int16')
+
+
+
+    left_sound = AudioSegment(
+        left_channel.tobytes(), 
+        frame_rate=left_channel_as.frame_rate,
+        sample_width=left_channel.dtype.itemsize, 
+        channels=1
+    )
+    right_sound = AudioSegment(
+        right_channel.tobytes(), 
+        frame_rate=right_channel_as.frame_rate,
+        sample_width=right_channel.dtype.itemsize, 
+        channels=1
+    )    
+
+    stereo_sound=AudioSegment.from_mono_audiosegments(left_sound, right_sound)
+    output_wav = os.path.join(OUTPUT_DIR, "1_sound.wav")
+    stereo_sound.export(output_wav)
+
+    try:
+        shutil.rmtree(tmp_path)
+        pass
+    except Exception as e:
+        print(print(str(e)))
 
     prep_plot(font_scale=1)
     plt.rcParams["figure.figsize"] = [16, 9]
 
-
-
     #PLOT
     if True:
-        fig, ax = plt.subplots(len(nyp_files))
+        fig, ax = plt.subplots(nb_nyp_files)
         for i, f in enumerate(nyp_files):
             print(f)
             Y=np.load(f)
@@ -169,23 +251,13 @@ def create_sound(nyp_files, output_dir):
 
     output_txt = os.path.join(OUTPUT_DIR, "0_intro.txt")
     txts = [
-        "And now let's transform today's data into a sound representation.",
+        "And now, let's transform today's data into an audio representation.",
         "Let's now listen to an audio representation of today's data.",
         "We shall now experience a sonified version of today's data.",
-        "And now, let's percieve data as a sound experience."
+        "To continue, let's percieve today's data as an audio experience."
     ]
     with open(output_txt, 'w') as f:
         f.write(random.choice(txts))
-
-
-    if DEBUG:
-        fig, ax = plt.subplots(len(YYs))
-        for i, Y in enumerate(YYs):
-            Y = (Y-Y.min())/(Y.max()-Y.min())
-            ax[i].plot(Y, color='magenta', linewidth=5)
-
-        plt.suptitle("Data to sound",  fontsize=30)    
-        plt.show()
 
 
 
